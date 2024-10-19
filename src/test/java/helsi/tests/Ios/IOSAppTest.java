e739125d8c2bf305e22375540b185ec2f7282e4f
xa6u <skarabuta@curacubby.com>
2024-10-19 14:55:22 +0300

    added ios tests draft

---
+++ src/test/java/helsi/tests/Ios/IOSAppTest.java
@@ -0,0 +1,47 @@
+package helsi.tests.Ios;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
+
+public class IOSAppTest {
+
+    private AppiumDriver driver;
+
+    @BeforeClass
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
+        caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "16.7"); // Версія iOS
+        caps.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone"); // Назва пристрою
+        caps.setCapability(MobileCapabilityType.UDID, "YOUR_DEVICE_UDID"); // UDID вашого пристрою
+        caps.setCapability(MobileCapabilityType.APP, "/path/to/your_app.ipa"); // Шлях до вашого .ipa або .app файлу
+        caps.setCapability("xcodeOrgId", "YOUR_XCODE_ORG_ID"); // Ідентифікатор організації Xcode
+        caps.setCapability("xcodeSigningId", "iPhone Developer"); // Метод підпису
+        caps.setCapability("automatorName", "XCUITest");
+
+        // Ініціалізація драйвера
+        driver = new IOSDriver(new URL("http://127.0.0.1:4723/wd/hub"), caps);
+    }
+
+    @Test
+    public void testAppLaunch() {
+        // Ваш код для тестування
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
