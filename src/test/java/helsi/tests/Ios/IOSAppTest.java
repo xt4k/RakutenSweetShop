78c7983c384c9d161e2cc75b5f9d2de7f40eb64f
xa6u <skarabuta@curacubby.com>
2024-10-20 17:04:24 +0300

    apple shit

--- src/test/java/helsi/tests/Ios/IOSAppTest.java
+++ src/test/java/helsi/tests/Ios/IOSAppTest.java
@@ -1,8 +1,11 @@
 package helsi.tests.Ios;
 
+import groovy.util.logging.Slf4j;
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -10,9 +13,19 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
+import static io.qameta.allure.Allure.step;
+
+
+@Tags({@Tag("anna"), @Tag("live")})
+@Slf4j
 public class IOSAppTest {
 
+    private static final Logger log = LoggerFactory.getLogger(IOSAppTest.class);
     private static AppiumDriver driver;
 
     @BeforeAll
@@ -20,20 +33,40 @@ public class IOSAppTest {
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
         caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "16.7"); // Версія iOS
-        caps.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone"); // Назва пристрою
-        caps.setCapability(MobileCapabilityType.UDID, "YOUR_DEVICE_UDID"); // UDID вашого пристрою
-        caps.setCapability(MobileCapabilityType.APP, "/path/to/your_app.ipa"); // Шлях до вашого .ipa або .app файлу
-        caps.setCapability("xcodeOrgId", "YOUR_XCODE_ORG_ID"); // Ідентифікатор організації Xcode
+        caps.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone X"); // Назва пристрою
+        caps.setCapability(MobileCapabilityType.UDID, "98c580f3abe2c0090797593faa1a9eee6f676637"); // UDID вашого пристрою
+        // caps.setCapability(MobileCapabilityType.APP,"com.google.Docs" );//"/path/to/your_app.ipa"); // Шлях до вашого .ipa або .app файлу
+        caps.setCapability("bundleId", "com.google.Docs");
+        caps.setCapability("xcodeOrgId", "Yuriy L"); // Ідентифікатор організації Xcode
         caps.setCapability("xcodeSigningId", "iPhone Developer"); // Метод підпису
-        caps.setCapability("automatorName", "XCUITest");
+        caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
+        caps.setCapability("showXcodeLog", true);
+
+
+
+        caps.setCapability(MobileCapabilityType.NO_RESET, true);
+        //caps.setCapability(MobileCapabilityType.FULL_RESET, true);
 
         // Ініціалізація драйвера
-        driver = new IOSDriver(new URL("http://127.0.0.1:4723/wd/hub"), caps);
+        driver = new IOSDriver(new URL("http://127.0.0.1:4723"), caps);//"http://127.0.0.1:4723/session"// "http://127.0.0.1:4723/wd/hub"
     }
+
+    //http://127.0.0.1:4723/wd/hub/status
 
     @Test
     public void testAppLaunch() {
-        // Ваш код для тестування
+        addListener("AllureSelenide", new AllureSelenide());
+       // driver.m m;;' m
+
+        open();
+        log.info("open notes");
+        step("open notes");
+
+        //    ((AndroidDriver) getWebDriver()).startRecordingScreen(new AndroidStartScreenRecordingOptions()
+        //            .withTimeLimit(Duration.ofSeconds(40)));
+      //  back();
+        //back();
+      //  $(AppiumBy.accessibilityId("HELSI")).click();
     }
 
     @AfterAll
