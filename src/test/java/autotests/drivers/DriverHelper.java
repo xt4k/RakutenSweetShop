e2ecb717dd93fb94a95c8e23ad9dd352e775fbf7
Yuriy L. <yuriy.logvinov@gmail.com>
2024-11-03 12:11:13 +0200

    changes driver config interface

--- src/test/java/autotests/drivers/DriverHelper.java
+++ src/test/java/autotests/drivers/DriverHelper.java
@@ -50,6 +50,11 @@ public class DriverHelper {
             chromeOptions.setExperimentalOption("prefs", prefs);
             capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
 
+            LOG.warn("====================================================================================");
+            LOG.warn("getDriverConfig  LT_USERNAME " + getDriverConfig().ltUserName2() +" size = "+getDriverConfig().ltUserName2().length());
+            LOG.warn("Environment variable LT_USERNAME: " + System.getenv("LT_USERNAME")+" size = "+System.getenv("LT_USERNAME").length());
+            LOG.warn("====================================================================================");
+
             if (!getDriverConfig().webRemoteDriverUrl().isBlank()) {
                 Map ltMap = Map.of(
                         "project", "Untitled",
