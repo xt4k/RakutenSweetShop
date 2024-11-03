9571cff6ea157be626a60b2a8e550ba7f747e295
Yuriy L. <yuriy.logvinov@gmail.com>
2024-11-03 12:04:14 +0200

    changes driver config interface

--- src/test/java/autotests/drivers/DriverHelper.java
+++ src/test/java/autotests/drivers/DriverHelper.java
@@ -51,11 +51,13 @@ public class DriverHelper {
             capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
 
             LOG.warn("====================================================================================");
-            LOG.warn("getDriverConfig ABC_DE " + getDriverConfig().webAbcde() +" size = "+getDriverConfig().webAbcde().length());
-            LOG.warn("system env ABC_DE " + getenv("ABC_DE") +" size = "+ getenv("ABC_DE").length());
+            LOG.warn("Environment variable srv: " + System.getenv("srv")+" size = "+System.getenv("srv").length());
             LOG.warn("getDriverConfig  LT_USERNAME " + getDriverConfig().ltUserName2() +" size = "+getDriverConfig().ltUserName2().length());
             LOG.warn("Environment variable LT_USERNAME: " + System.getenv("LT_USERNAME")+" size = "+System.getenv("LT_USERNAME").length());
-            LOG.warn("Environment variable LT_USERNAME: " + System.getenv("srv")+" size = "+System.getenv("srv").length());
+
+            LOG.warn("getDriverConfig ABC_DE " + getDriverConfig().webAbcde() +" size = "+getDriverConfig().webAbcde().length());
+            LOG.warn("system env ABC_DE " + getenv("ABC_DE") +" size = "+ getenv("ABC_DE").length());
+
             LOG.warn("====================================================================================");
 
             if (!getDriverConfig().webRemoteDriverUrl().isBlank()) {
