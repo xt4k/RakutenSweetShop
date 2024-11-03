bbc87d033c0ddbf88c75657a7fa19f9d2db132a2
Yuriy L. <yuriy.logvinov@gmail.com>
2024-11-03 11:54:12 +0200

    changes driver config interface

--- src/test/java/autotests/config/DriverConfig.java
+++ src/test/java/autotests/config/DriverConfig.java
@@ -4,8 +4,8 @@ import org.aeonbits.owner.Config;
 
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
-        "system:properties",
         "system:env",
+        "system:properties",
         "classpath:config/${srv}_driver.properties",
         "classpath:config/app.properties"
 })
